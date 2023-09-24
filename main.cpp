b2dfe3c04629dcb2f33e2998e1c4883c029f6833
Zsolt5991 <55974837+Zsolt5991@users.noreply.github.com>
2023-09-24 20:47:31 +0200

    Update main.cpp
    hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];  // el van írva
+    std::cout << '1-100 ertekek duplazasa' // pontosvessző hiánya és " kell  használni nem '
+    for (int i = 0;)  // hiányos for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)   //  i ig megy a ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"     // pontosvessző hiánya
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
+    std::cout << "Atlag szamitasa: " << std::endl;    
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++)     // nem vessző kell hanem ;
     {
-        atlag += b[i]
+        atlag += b[i]    // pontosvessző hiánya
     }
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
+    std::cout << "Atlag: " << atlag << std::endl;  
     return 0;
 }
